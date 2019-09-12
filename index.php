27483d9957cecf0e32052167609ad649126e8ca8
Fikus1020 <45180885+Fikus1020@users.noreply.github.com>
2019-09-12 14:33:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <div class="container">
+        <div class="header">header</div>
+        <div class="sidebar">sidebar</div>
+        <div class="item card">1</div>
+        <div class="item card">2</div>
+        <?php require("card.php"); ?>
+        <div class="item card">4</div>
+        <div class="item card">5</div>
+        <div class="item card">6</div>
+        <div class="item card">7</div>
+        <div class="item card">8</div>
+        <div class="item card">9</div>
+        <div class="item card">10</div>
+        <div class="item card">11</div>
+        <div class="item card">12</div>
+        
+        <div class="footer">4</div>
+    </div>
+</body>
+</html>
