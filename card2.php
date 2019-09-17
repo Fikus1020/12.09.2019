3fe3eab0909f57b0f63aa0e6945a480b5bc90558
Fikus1020 <45180885+Fikus1020@users.noreply.github.com>
2019-09-17 15:42:44 +0200

    Create card2.php

---
+++ card2.php
@@ -0,0 +1,17 @@
+<?php
+echo('
+<div class="item card">
+<div class="info">
+    <img class="photo" src="jeep2.img">
+    <p class="price">Cena: <span class="price-value">217$</span></p>
+    <span class="promo">Promocja</span>
+    <div class="size">Kolor:
+        <ul class="list">
+            <li class="size-item">Szybki</li>
+            <li class="size-item">Szybszy</li>
+            <li class="size-item">Wolniejszy</li>
+        </ul>
+    </div>
+</div>
+</div>');
+?>
